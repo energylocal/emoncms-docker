bbc5cdad09efb22c8f7598b3d43284d202e9652c
Luke Farrar <lukepfarrar@gmail.com>
2024-02-08 14:52:45 +0000

    Script to run demand shapers as in live crontab

---
+++ run_demandshapers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/bethesda.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/corwen.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/crickhowell.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/repower.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/machynlleth.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/repower.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/bridport.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/llandysul.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/test.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/milfordhaven.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/roupellpark.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/capeldewi.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/dyffrynbanw.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/economy7.php
+docker compose exec web /usr/local/bin/php /opt/emoncms/modules/cydynni/scripts/demandshapers/totnes.php
